dc28548d20b3493e09d282397bdc145d1fd78d34
kunozga265 <kunozgamlowoka@gmail.com>
2022-06-15 17:13:22 +0200

    Added about and added logout routes

--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -27,14 +27,32 @@
                     <p class="headline">{{user.info.name}}</p>
                     <p>{{user.data.email}}</p>
                     <p>{{user.info.number1}}</p>
-                    <v-btn
+<!--                    <v-btn
                         text
                         @click="logout"
                         color="blue darken-3"
                     >
                         Logout
-                    </v-btn>
+                    </v-btn>-->
                 </div>
+
+                <div class="text-center mt-5">
+                    <div class="title blue--text darken-3">About</div>
+                    Developed and supported by LUANAR & MSU teams led by Dr. Samson Katengeza, Prof Emmanuel Kaunda, Dr. Abigail Bennet, Edith Gondwe, Emma Rice, Patrick Chimseu, Gehazi Jenda and Kunozga Mlowoka with input from many other colleagues and stakeholders.
+                </div>
+
+                <div class="text-center mt-10">
+                    <div v-show="user.type==='Trader'" class="mb-3 f-button blue darken-3" @click="iWantToBuy">
+                        I Want To Buy
+                    </div>
+                    <div v-show="user.type==='Customer'" class="mb-3 f-button blue darken-3" @click="iWantToSell">
+                        I Want To Sell
+                    </div>
+                    <div class="mb-3 f-button blue darken-3" @click="logout">
+                        Logout
+                    </div>
+                </div>
+
 
             </v-card>
         </v-layout>
@@ -79,7 +97,20 @@ export default {
                 this.$store.dispatch("logout");
                 this.$router.replace({name:'welcome'})
             })
+        },
+        iWantToSell () {
+            signOut(auth).then(() => {
+                this.$store.dispatch("logout");
+                this.$router.replace({name:'i-want-to-sell'})
+            })
+        },
+        iWantToBuy () {
+            signOut(auth).then(() => {
+                this.$store.dispatch("logout");
+                this.$router.replace({name:'i-want-to-buy'})
+            })
         }
+
     }
 
 }
